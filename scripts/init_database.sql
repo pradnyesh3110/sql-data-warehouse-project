5c2fb00aa772b05033169d6c878b2e65725973c8
pradnyesh3110 <157879214+pradnyesh3110@users.noreply.github.com>
2025-04-08 00:06:34 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,10 @@
+/* create database and schema 
+--- creating new dataset Datawarehouse. it contains 3 layers bronze,silver,gold....
+
+create database DataWarehouse;
+use Datawarehouse;
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
